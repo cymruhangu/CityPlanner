a43c29a7a6d31124f0113ba0eb599c8c19981609
cymruhangu <pragquix@gmail.com>
2018-06-12 19:46:45 -0400

    6/12 mentor session. Modularized main.js

--- main.js
+++ main.js
@@ -1,66 +1,59 @@
-(function(){
+myNameSpace = function(){
 // 'use strict';
 const mapsAPIKey = 'AIzaSyBFyC3jDrzJK-9cl0wuWZonC-JpwP5Gaho';
+let map = null;
 
 // ===================
 function initMap() {
-        var map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: 40.7829, lng: -73.9654},
-          zoom: 13
-        });
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 40.7829, lng: -73.9654},
+    zoom: 13
+  });
 
-        var input = document.getElementById('pac-input');
+  var input = document.getElementById('pac-input');
 
-        var autocomplete = new google.maps.places.Autocomplete(input);
-        autocomplete.bindTo('bounds', map);
+  var autocomplete = new google.maps.places.Autocomplete(input);
+  autocomplete.bindTo('bounds', map);
 
-        map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
-        var infowindow = new google.maps.InfoWindow();
-        var infowindowContent = document.getElementById('infowindow-content');
-        infowindow.setContent(infowindowContent);
-        var marker = new google.maps.Marker({
-          map: map
-        });
-        marker.addListener('click', function() {
-          infowindow.open(map, marker);
-        });
+  var infowindow = new google.maps.InfoWindow();
+  var infowindowContent = document.getElementById('infowindow-content');
+  infowindow.setContent(infowindowContent);
+  var marker = new google.maps.Marker({
+    map: map
+  });
+  marker.addListener('click', function() {
+    infowindow.open(map, marker);
+  });
 
-        autocomplete.addListener('place_changed', function() {
-          infowindow.close();
-          var place = autocomplete.getPlace();
-          if (!place.geometry) {
-            return;
-          }
+  autocomplete.addListener('place_changed', function() {
+    infowindow.close();
+    var place = autocomplete.getPlace();
+    if (!place.geometry) {
+      return;
+    }
 
-          if (place.geometry.viewport) {
-            map.fitBounds(place.geometry.viewport);
-          } else {
-            map.setCenter(place.geometry.location);
-            map.setZoom(13);
-          }
+    // Set the position of the marker using the place ID and location.
+    marker.setPlace({
+      placeId: place.place_id,
+      location: place.geometry.location
+    });
+    marker.setVisible(true);
 
-          // Set the position of the marker using the place ID and location.
-          marker.setPlace({
-            placeId: place.place_id,
-            location: place.geometry.location
-          });
-          marker.setVisible(true);
+    infowindowContent.children['place-name'].textContent = place.name;
+    // infowindowContent.children['place-id'].textContent = place.place_id;
+    infowindowContent.children['place-address'].textContent =
+      place.formatted_address;
+    infowindow.open(map, marker);
+  });
+}
 
-          infowindowContent.children['place-name'].textContent = place.name;
-          infowindowContent.children['place-id'].textContent = place.place_id;
-          infowindowContent.children['place-address'].textContent =
-              place.formatted_address;
-          infowindow.open(map, marker);
-        });
-      }
-
-      
 $.ajax({
   url: "https://maps.googleapis.com/maps/api/geocode/json",
   type: "GET",
   data: {
-    address: 'Smoke Jazz NY',
+    address: 'Penn Station',
     key: `${mapsAPIKey}`
   },
   success: function(data) {
@@ -68,5 +61,7 @@ $.ajax({
     console.log(`placeID is ${placeID}`);
   }
 });
-
-})();
+  return{
+    initMap:initMap
+  }
+}();
