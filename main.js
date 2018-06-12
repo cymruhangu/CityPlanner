d0c1f0b56428ffd1e91a3ae24ecc0da998f70a85
cymruhangu <pragquix@gmail.com>
2018-06-12 06:26:40 -0400

    6/11 evening

--- main.js
+++ main.js
@@ -1,23 +1,72 @@
 (function(){
-'use strict';
+// 'use strict';
 const mapsAPIKey = 'AIzaSyBFyC3jDrzJK-9cl0wuWZonC-JpwP5Gaho';
-const placesAPIKey = ''
 
+// ===================
+function initMap() {
+        var map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: 40.7829, lng: -73.9654},
+          zoom: 13
+        });
+
+        var input = document.getElementById('pac-input');
+
+        var autocomplete = new google.maps.places.Autocomplete(input);
+        autocomplete.bindTo('bounds', map);
+
+        map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+        var infowindow = new google.maps.InfoWindow();
+        var infowindowContent = document.getElementById('infowindow-content');
+        infowindow.setContent(infowindowContent);
+        var marker = new google.maps.Marker({
+          map: map
+        });
+        marker.addListener('click', function() {
+          infowindow.open(map, marker);
+        });
+
+        autocomplete.addListener('place_changed', function() {
+          infowindow.close();
+          var place = autocomplete.getPlace();
+          if (!place.geometry) {
+            return;
+          }
+
+          if (place.geometry.viewport) {
+            map.fitBounds(place.geometry.viewport);
+          } else {
+            map.setCenter(place.geometry.location);
+            map.setZoom(13);
+          }
+
+          // Set the position of the marker using the place ID and location.
+          marker.setPlace({
+            placeId: place.place_id,
+            location: place.geometry.location
+          });
+          marker.setVisible(true);
+
+          infowindowContent.children['place-name'].textContent = place.name;
+          infowindowContent.children['place-id'].textContent = place.place_id;
+          infowindowContent.children['place-address'].textContent =
+              place.formatted_address;
+          infowindow.open(map, marker);
+        });
+      }
+
+      
 $.ajax({
-  url: "https://maps.googleapis.com/maps/api/distancematrix/json",
+  url: "https://maps.googleapis.com/maps/api/geocode/json",
   type: "GET",
   data: {
-    origins: $("#origin").val(),
-    destination: $("#destinations").val(),
-    libraries: 'places'
-    key: mapsAPIKey
+    address: 'Smoke Jazz NY',
+    key: `${mapsAPIKey}`
   },
   success: function(data) {
-    console.log(data);
+    let placeID = data.results[0].place_id;
+    console.log(`placeID is ${placeID}`);
   }
 });
-
-
-
 
 })();
