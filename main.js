6927fcb8c3e6af78504c99acb434d5a78b54b4a5
cymruhangu <pragquix@gmail.com>
2018-06-17 15:51:46 -0400

    markers and infowindows added for each selection. Added splash form but no handler

--- main.js
+++ main.js
@@ -1,15 +1,39 @@
 mySpace = function(){
 'use strict';
 const mapsAPIKey = 'AIzaSyBFyC3jDrzJK-9cl0wuWZonC-JpwP5Gaho';
+const cityCenters = [
+        { city: "NY",
+          center: {lat: 40.7829, lng: -73.9654}
+        },
+        { city: "PHL",
+          center: {lat: 39.9526, lng: -75.1652}
+        },
+        { city: "BSTN",
+          center: {lat: 42.3601, lng: -71.0589}
+        },
+        { city: "DC",
+          center: {lat: 38.9072, lng: -77.0369}
+        },
+        { city: "CHI",
+          center: {lat: 41.8781, lng: -87.6298}
+        },
+];
 let map = null;
 let marker = null;
 let myPlaces = [];
 let placeIndex = -1;
+let cityCenter = {lat: 40.7829, lng: -73.9654};
 // ===================
+/* 
+  - map is hidden at start
+  - default city is NY
+  - get selected City and dates from form
+  - Call initMap again with the new city and unhide
+*/
 
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 40.7829, lng: -73.9654},
+    center: cityCenter,
     zoom: 12,
     gestureHandling: 'cooperative'
   });
@@ -26,29 +50,29 @@ function placeSelection(){
 
   map.controls[google.maps.ControlPosition.TOP_CENTER].push(input);  
   
-  let infowindow = new google.maps.InfoWindow();
-  let infowindowContent = document.getElementById('infowindow-content');
-  infowindow.setContent(infowindowContent);
-  marker = new google.maps.Marker({
-    map: map
-  });
-  marker.addListener('click', function() {
-    infowindow.open(map, marker);
-  });
+  // let infowindow = new google.maps.InfoWindow();
+  // let infowindowContent = document.getElementById('infowindow-content');
+  // infowindow.setContent(infowindowContent);
+  // marker = new google.maps.Marker({
+  //   map: map
+  // });
+  // marker.addListener('click', function() {
+  //   infowindow.open(map, marker);
+  // });
 
   autocomplete.addListener('place_changed', function() {
-    infowindow.close();
+    // infowindow.close();
     let place = autocomplete.getPlace();
     if (!place.geometry) {
       return;
     }
 
     // Set the position of the marker using the place ID and location.
-    marker.setPlace({
-      placeId: place.place_id,
-      location: place.geometry.location
-    });
-    marker.setVisible(true);
+    // marker.setPlace({
+    //   placeId: place.place_id,
+    //   location: place.geometry.location
+    // });
+    // marker.setVisible(true);
     
     let selected = new Place(place.name, place.formatted_address, place.place_id,
      place.formatted_phone_number, place.website, place.reviews, place.rating, place.price_level);
@@ -57,13 +81,7 @@ function placeSelection(){
     placeIndex++;
     //set the coords for new place object
     setCoords(placeIndex);
-    console.log(`myPlaces lat: ${myPlaces[placeIndex].lat}`);
-    //create marker and infowindow
-    // newMarker(placeIndex);
-    
     updatePlaces();
-    console.log(myPlaces[placeIndex]);
-    console.log(myPlaces[placeIndex].lat);
     ///clear the autocomplete input
     document.getElementById('pac-input').value = '';
   });
@@ -81,23 +99,7 @@ function Place (name, address, placeID, phone, website, reviews, rating, price){
   this.price = price;
 }
 
-// function addMarker(index){
-//   let x = places[index].name;
-//   let y = places[index].lng;
-//   console.log(` coords: {lat: ${x}, lng:${y}}`);
-  // let marker = new google.maps.Marker({
-  //   position: `{lat: ${places[index].lat}, lng:${places[index].lng}}`,
-  //   map:map
-  // });
-  
-  // let infoWindow = new google.maps.InfoWindow({
-  //   content: 'blah'
-  // });
 
-  // marker.addListener('click', function(){
-  //   infoWindow.open(map, marker);
-  // });
-// s}
 
 function updatePlaces(){
   let list = '';
@@ -121,13 +123,36 @@ function setCoords(index){
     success: function(data) {
       let foundLat = data.results[0].geometry.location.lat;
       let foundLng = data.results[0].geometry.location.lng;
-      // console.log(data);
-      console.log(`Coordinates are lat: ${foundLat} long: ${foundLng}`);
       myPlaces[index].lat = foundLat;
       myPlaces[index].lng = foundLng;
-    }
+      addMarker(index);
+    } 
+    //add error
   });
 }
 
+function addMarker(index){
+  let x = myPlaces[index].lat;
+  let y = myPlaces[index].lng;
+  let coordsObj = {lat:x,lng:y};
+  let marker = new google.maps.Marker({
+    position: coordsObj,
+    map:map
+  });
+  
+  let contentStr = `${myPlaces[index].name}
+                    <br>
+                    ${myPlaces[index].address}
+                    `;
+  let infoWindow = new google.maps.InfoWindow({
+    content: contentStr
+  });
+
+  marker.addListener('click', function(){
+    infoWindow.open(map, marker);
+  });
+}
+
+//==========================
 return {initMap:initMap}
 }();
